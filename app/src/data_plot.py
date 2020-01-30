6a5188c0aedc2477dc0fd7eaae0d1ad270577f8d
NanaShirakashi <haribo.0730@gmail.com>
2020-01-30 13:00:08 +0900

    データ量の可視化

---
+++ app/src/data_plot.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+plt.figure(figsize=(4,3),facecolor = "white")
+
+test_path = '../data/test/'
+l1 = []
+for dir_path in os.listdir(test_path):
+    target_dir = test_path + "/" + dir_path
+    files = os.listdir(target_dir)
+    count = len(files)
+    l1.append(count)
+
+train_path = '../data/train/'
+l2 = []
+for dir_path in os.listdir(train_path):
+    target_dir = train_path + "/" + dir_path
+    files = os.listdir(target_dir)
+    count = len(files)
+    l2.append(count)
+
+
+Y1 = np.array(l1)
+Y2 = np.array(l2)
+
+X = np.arange(len(Y1))
+
+w = 0.3
+
+plt.figure(figsize=(15,6))
+
+plt.bar(X + w,Y1,color='g',width=w,label='test',align ="center")
+plt.bar(X,Y2,color='b',width=w,label='train',align ="center")
+
+plt.legend(loc="best")
+
+path = '../data/test'
+files = os.listdir(path)
+
+plt.xticks(X + w/2,files)
+plt.grid(True)
+
+plt.show()
+
+
